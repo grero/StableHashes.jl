0126c81e8614b544cbf5dbed11ca6fab638576fb
Roger Herikstad <roger.herikstad@gmail.com>
2020-09-22 16:12:52 +0800

    Includes container types and exports shash

--- src/StableHashes.jl
+++ src/StableHashes.jl
@@ -1,6 +1,12 @@
 module StableHashes
 
+include("intfuncs.jl")
 include("hashing.jl")
+include("float.jl")
 include("hashing2.jl")
+include("irrationals.jl")
+include("pair.jl")
+include("array.jl")
 
+export shash
 end # module
